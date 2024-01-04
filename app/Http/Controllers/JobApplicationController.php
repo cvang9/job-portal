ab1e4e59c6c3d2a2637070f1859b8f59a65ecd2c
Shivang <harshsaini5656@gmail.com>
2024-01-04 23:16:11 +0530

    added file uplaod

--- app/Http/Controllers/JobApplicationController.php
+++ app/Http/Controllers/JobApplicationController.php
@@ -17,11 +17,20 @@ class JobApplicationController extends Controller
     public function store(Request $request, Job $job)
     {
         $this->authorize('apply', $job );
+
+        $validated = $request->validate([
+            'expected_salary' => 'required|min:1|max:1000000',
+            'cv' => 'required|file|mimes:pdf|max:2048'
+        ]);
+
+        $file = $request->file('cv');
+        $path = $file->store('cvs', 'private' );
+
+
         $job->jobApplications()->create([
             'user_id' => auth()->user()->id,
-            ...$request->validate([
-                'expected_salary' => 'required|min:1|max:1000000'
-            ])
+            'expected_salary' => $validated['expected_salary'],
+            'cv_path' => $path
         ]);
 
         return redirect()->route('jobs.show', ['job' => $job] )->with('success', 'Job Application Successfully Submitted');
--- resources/views/job_application/create.blade.php
+++ resources/views/job_application/create.blade.php
@@ -5,10 +5,11 @@
     <x-card>
         <h2 class="text-3xl mb-4 text-center"> Your Job Application </h2>
         <div class="form">
-            <form action="{{ route('jobs.application.store', ['job' => $job ]) }}" method="post">
+            <form action="{{ route('jobs.application.store', ['job' => $job ]) }}" method="post" enctype="multipart/form-data">
                 @csrf
 
                 <x-text-input type="number" name="expected_salary" placeholder="Your Expected Salary" value="" />
+                <input type="file" name="cv" />
                 <button style="margin-left: 48%" type="submit" class="mt-4 border border-blue-500 py-1 px-4 text-blue-500 "> Apply </button>
             </form>
         </div>
